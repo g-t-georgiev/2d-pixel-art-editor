7dc09a1fbd723223d5c180168d8ab6dbdfa06b3f
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2026-08-17 18:56:49 +0300

    fix: handle null color data properly
    - implement seprate "color" type (string | null)
    - remove falsy color data guards to allow null (transparent) color by tools and painting pipeline

--- src/modules/utils/ColorUtils.ts
+++ src/modules/utils/ColorUtils.ts
@@ -1,10 +1,12 @@
+import type { color } from "../types";
+
 export default class ColorUtils {
-  static hexToRgba(hex: string | null) {
-    if (!hex) return { r: 0, g: 0, b: 0, a: 0 };
+  static hexToRgba(value: color) {
+    if (!value) return { r: 0, g: 0, b: 0, a: 0 };
 
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    const fullHex = hex.replace(shorthandRegex, (_, r, g, b) => r + r + g + g + b + b);
+    const fullHex = value.replace(shorthandRegex, (_, r, g, b) => r + r + g + g + b + b);
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(fullHex);
 
     return result ? {
